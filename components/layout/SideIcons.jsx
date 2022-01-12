020383f2cea50d138e7e3073697a71eb064fbf05
maxinth <tunde.j.adeleye@gmail.com>
2022-01-12 14:47:44 +0100

    styled sideIcons

--- components/layout/SideIcons.jsx
+++ components/layout/SideIcons.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import whatsAppSupport from "../../public/images/quick-contact.svg";
 import chat from "../../public/images/chat.svg";
-import scrollUp from "../../public/images/scroll-up.svg";
 import Image from "next/image";
 import styles from "../../public/css/Signup.module.scss";
 
 const SideIcons = ({ children }) => {
-  console.log("styles = ", styles);
-  console.log("from SideIcons");
   return (
     <section className={styles.container}>
-      <div className={`${styles.icon} ${styles.boxOne}`}>
+      <a
+        className={`${styles.icon} ${styles.boxOne}`}
+        href="https://wa.me/+2349016324945"
+      >
         <Image
           src={whatsAppSupport}
-          width="54"
-          height="54"
+          width="64"
+          height="64"
           className="whatsapp"
         />
-      </div>
+      </a>
       {children}
       <div className={`${styles.icon} ${styles.boxTwo}`}>
-        <Image src={chat} width="54" height="54" />
-        <Image src={scrollUp} width="54" height="54" />
+        <Image src={chat} width="64" height="54" />
       </div>
     </section>
   );
