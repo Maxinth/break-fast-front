a4bd73e8de77c4a3388e41a78064c0f0af5e09ed
maxinth <tunde.j.adeleye@gmail.com>
2022-01-14 10:38:34 +0100

    new updates

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ import "react-dates/initialize";
 import "antd/dist/antd.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
@@ -20,9 +22,18 @@ function MyApp({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
 
   useEffect(() => {
+    // load AOS for animations
+    AOS.init();
+    AOS.refresh();
+    console.log("aos has loaded!");
+  }, []);
+
+  useEffect(() => {
+    // set authHeaders for authorizations
     setAuthorizationHeader();
     console.log("authorizationHeader set!");
   }, []);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
